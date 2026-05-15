ff3ff7640d477b07d5a59d834a8cfeef67a13f21
Olavo <olavo.goncalves2005@gmail.com>
2026-05-15 22:05:04 +0100

    Funcionalidade da pagina home com adicionar nos favoritos

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,87 +6,108 @@ import AppFooter from "../components/AppFooter";
 import AppHeader from "../components/AppHeader";
 import { homeStyles } from '../styles/homeScreenStyles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import data from '../../data/properties.json'
+import { useState } from 'react';
+import { addLiked } from '../storage/likedStorage';
 
 
-const HomeScreen = ({navigation}: any) => (
-    <View style={globalStyles.screen}>
-        <AppHeader navigation={navigation}/>
-        <View style={[globalStyles.centeredContainer, {paddingVertical: 16}]}> 
-            <View style={homeStyles.cardContainer}>
-                <ImageBackground
-                    source={require('../../assets/house.png')}
-                    style={homeStyles.card}
-                    imageStyle={homeStyles.cardImage}
-                >
-                    <View style={homeStyles.overlay}>
+const HomeScreen = ({navigation}: any) => {
+    
+    const [index, setIndex] = useState(0);
+
+    const addToLiked = async () => {
+        setIndex(i => i + 1);
+        addLiked({
+            propertyCode: data[index].propertyCode,
+            image: data[index].thumbnail,
+            province: data[index].province,
+            municipality: data[index].municipality,
+            address: data[index].address,
+            rooms: data[index].rooms,
+            bathrooms: data[index].bathrooms,
+            size: data[index].size,
+            price: data[index].price,
+        })
+    }
+    
+    return (
+        <View style={globalStyles.screen}>
+            <AppHeader navigation={navigation}/>
+            <View style={[globalStyles.centeredContainer, {paddingVertical: 16}]}> 
+                <View style={homeStyles.cardContainer}>
+                    <ImageBackground
+                        source={{uri: data[index].thumbnail}} // -- IMAGE -- 
+                        style={homeStyles.card}
+                        imageStyle={homeStyles.cardImage}
+                    >
+                        <View style={homeStyles.overlay}>
 
 
-                        <View>
-                            <View style={homeStyles.locationBadge}>
-                                <Icon name="location-on" size={16} color={COLORS.branco} />
-                                <Text style={homeStyles.locationBadgeText}>Porto, Portugal</Text>
-                            </View>
-                            <Text style={homeStyles.title}>T1 + 1 em V.N de Gaia</Text>
-                        </View>
-
-                        <View>
-                            <View style={homeStyles.bottomContent}>
-
-                            <View style={homeStyles.addressContainer}>
-                                <View style={homeStyles.addressRow}>
+                            <View>
+                                <View style={homeStyles.locationBadge}>
                                     <Icon name="location-on" size={16} color={COLORS.branco} />
-                                    <Text style={homeStyles.addressText}>
-                                        Rua de Digo Macedo, 51
-                                    </Text>
+                                    <Text style={homeStyles.locationBadgeText}>{data[index].province}, Portugal</Text> {/* -- PROVINCE -- */}
+                                </View>
+                                <Text style={homeStyles.title}>T{data[index].rooms} em V.N de Gaia</Text> {/* -- ROOMS -- */} 
+                            </View>
+
+                            <View>
+                                <View style={homeStyles.bottomContent}>
+
+                                <View style={homeStyles.addressContainer}>
+                                    <View style={homeStyles.addressRow}>
+                                        <Icon name="location-on" size={16} color={COLORS.branco} />
+                                        <Text style={homeStyles.addressText}>{data[index].address}</Text> {/* -- ADDRESS -- */}
+                                    </View>
+
+                                    <Text style={homeStyles.cityText}>{data[index].municipality}</Text> {/* -- MUNICIPALITY -- */}
                                 </View>
 
-                                <Text style={homeStyles.cityText}>Vila Nova de Gaia</Text>
+                                <View style={homeStyles.priceContainer}>
+                                    <Text style={homeStyles.price}>€{data[index].price}</Text> {/* -- PRICE -- */}
+                                    <Text style={homeStyles.priceMonth}>por mês</Text>
+                                </View>
                             </View>
+                            <View style={homeStyles.infoRow}>
 
-                            <View style={homeStyles.priceContainer}>
-                                <Text style={homeStyles.price}>€1.450</Text>
-                                <Text style={homeStyles.priceMonth}>por mês</Text>
+                                <View style={homeStyles.infoItem}>
+                                    <Icon name="bed" size={20} color={COLORS.branco} />
+                                    <Text style={homeStyles.infoText}>{data[index].rooms}</Text> {/* -- ROOMS -- */}
+                                </View>
+                                <View style={homeStyles.infoItem}>
+                                    <Icon name="bathtub" size={20} color={COLORS.branco} />
+                                    <Text style={homeStyles.infoText}>{data[index].bathrooms}</Text> {/* -- BATHROOMS -- */}
+                                </View>
+
+                                <View style={homeStyles.infoItem}>
+                                    <Icon name="square-foot" size={20} color={COLORS.branco} />
+                                    <Text style={homeStyles.infoText}>{data[index].size}m²</Text> {/* -- SIZE -- */}
+                                </View>
+
                             </View>
+                            </View>
+                            
+
+                            
                         </View>
-                        <View style={homeStyles.infoRow}>
+                    </ImageBackground>
+                </View>
+                <View style={homeStyles.actionsContainer}>
 
-                            <View style={homeStyles.infoItem}>
-                                <Icon name="bed" size={20} color={COLORS.branco} />
-                                <Text style={homeStyles.infoText}>2</Text>
-                            </View>
-                            <View style={homeStyles.infoItem}>
-                                <Icon name="bathtub" size={20} color={COLORS.branco} />
-                                <Text style={homeStyles.infoText}>1</Text>
-                            </View>
+                    <TouchableOpacity style={homeStyles.rejectButton} onPress={() => setIndex(i => i + 1)}>
+                        <Icon name="close" size={34} color="#D32F2F" />
+                    </TouchableOpacity>
 
-                            <View style={homeStyles.infoItem}>
-                                <Icon name="square-foot" size={20} color={COLORS.branco} />
-                                <Text style={homeStyles.infoText}>85m²</Text>
-                            </View>
+                    <TouchableOpacity style={homeStyles.favoriteButton} onPress={() => addToLiked()}>
+                        <Icon name="favorite" size={30} color={COLORS.branco} />
+                    </TouchableOpacity>
 
-                        </View>
-                        </View>
-                        
-
-                        
-                    </View>
-                </ImageBackground>
+                </View>
             </View>
-             <View style={homeStyles.actionsContainer}>
-
-                <TouchableOpacity style={homeStyles.rejectButton}>
-                    <Icon name="close" size={34} color="#D32F2F" />
-                </TouchableOpacity>
-
-                <TouchableOpacity style={homeStyles.favoriteButton}>
-                    <Icon name="favorite" size={30} color={COLORS.branco} />
-                </TouchableOpacity>
-
-            </View>
+            <AppFooter navigation={navigation} activeScreen="Home"/>
         </View>
-        <AppFooter navigation={navigation} activeScreen="Home"/>
-    </View>
-)
+    );
+};
 
 
 export default HomeScreen;
